1f4599b57e6e171b40439748d8359c9bb25bae03
Santhu Rao <raoraosanthu@gmail.com>
2022-01-23 20:24:02 +0000

    stage branch

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "octopus-tf-state"
-    key    = "development/octopus-stage.tfstate"
+    key    = "stage/octopus-stage.tfstate"
     region = "eu-west-2"
   }
 }
